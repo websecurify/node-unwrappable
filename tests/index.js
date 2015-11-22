5b53fcb7bebe900363070d781c2c959cb876df6a
Petko D. Petkov <pdp@websecurify.com>
2015-11-22 21:41:14 +0000

    Fixed bug with function call.

--- tests/index.js
+++ tests/index.js
@@ -23,3 +23,38 @@ exports.t02 = function (test) {
     test.ok(f() === 'abc')
     test.done()
 }
+
+exports.t03 = function (test) {
+    var f = function (message) { return message }
+
+    f = unwrappable(f)
+    f = unwrappable(f)
+    f = unwrappable(f)
+
+    test.ok(f('hello') === undefined)
+    test.ok(f('hello') === undefined)
+    test.ok(f('hello') === undefined)
+    test.ok(f('hello') === 'hello')
+    test.ok(f('hello') === 'hello')
+    test.ok(f('hello') === 'hello')
+    test.done()
+}
+
+exports.t04 = function (test) {
+    var f = function () { return Array.prototype.slice.call(arguments).join(' ') }
+
+    f = unwrappable(f)
+    f = unwrappable(f)
+    f = unwrappable(f)
+    f = unwrappable(f)
+
+    test.ok(f('hello', 'world') === undefined)
+    test.ok(f('hello', 'world') === undefined)
+    test.ok(f('hello', 'world') === undefined)
+    test.ok(f('hello', 'world') === undefined)
+    test.ok(f('hello', 'world') === 'hello world')
+    test.ok(f('hello', 'world') === 'hello world')
+    test.ok(f('hello', 'world') === 'hello world')
+    test.ok(f('hello', 'world') === 'hello world')
+    test.done()
+}
